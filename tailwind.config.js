f60956d8bdb39bda3879aafaf7138a1a7f8bd1f8
take-1019 <tkchtmy@gmail.com>
2022-09-25 12:17:57 +0900

    🎉

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	theme: { extend: {} },
+	plugins: [require("@tailwindcss/typography")],
 };
